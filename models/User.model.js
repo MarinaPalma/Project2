0e999ec32f3cd70d48aec7b242062228e21ed709
Marina Palma <marina_palma@hotmail.com>
2022-05-13 16:54:09 +0100

    signup update

--- models/User.model.js
+++ models/User.model.js
@@ -5,17 +5,17 @@ const userSchema = new Schema({
     type: String,
     trim: true,
     require: true,
-    travels: [],
-    imageUrl: {
-      type: String,
-      default: "",
-    },
-  },
 
+  },
+  travels: [{type: Schema.Types.ObjectId, ref: "Travel"}],
+  imageUrl: {
+    type: String,
+    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fhu%2Fsearch%2Fimages%3Fk%3Ddefault%2Bprofile%2Bpicture&psig=AOvVaw3xK-BkapZQRY_3-yRm8GxQ&ust=1652543320992000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJiwu9_p3PcCFQAAAAAdAAAAABAD",
+  },
   email: {
     type: String,
     required: [true, "Please enter a username"],
-    unique: true,
+    // unique: true,
     trim: true,
     lowercase: true,
     // match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
