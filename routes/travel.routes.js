824177af7927868e14c1addbd2105a9abea0982b
Alexandre Alves Fernandes Junior <alexandrealvesfj@gmail.com>
2022-05-16 10:44:07 +0100

    Travel routes done

--- routes/travel.routes.js
+++ routes/travel.routes.js
@@ -21,4 +21,28 @@ router.post("/travels/create", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+router.get("/travels/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  Travel.findById(id)
+    .then((travels) => {
+      res.render("travels/travel-edit", travels);
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/travels/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  const { title, country, city, date, description } = req.body;
+  Travel.findByIdAndUpdate(id, { title, country, city, date, description })
+    .then(() => res.redirect("/travels"))
+    .catch((err) => next(err));
+});
+
+router.post("/travels/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+  Travel.findByIdAndRemove(id)
+    .then(() => res.redirect("/travels"))
+    .catch((err) => next(err));
+});
+
 module.exports = router;
