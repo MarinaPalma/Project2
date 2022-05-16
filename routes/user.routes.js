d7071efb654a5886797c85583da4b896b8ca6c6e
Marina Palma <marina_palma@hotmail.com>
2022-05-16 10:46:24 +0100

    user edit

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,5 +9,33 @@ router.get("/profile", (req, res, next) => {
       
   });
 
+router.get('/profile/:id/edit', (req, res, next) => {
+    const { id } = req.params;
+    User.findById(id)
+      .then((user) => {
+        res.render('users/profile-edit', user);
+      })
+      .catch((err) => next(err));
+  });
+
+//   router.post('/:id/edit', fileUploader.single('profile-image'), (req, res, next) => {
+//     const { id } = req.params;
+
+//     const { username } = req.body;
+   
+  
+//     if (req.file) {
+//       User.findByIdAndUpdate(id, { username, imageUrl: req.file.path })
+//         .then(() => res.redirect('/profile'))
+//         .catch((err) => next(err));
+//     } else {
+//       User.findByIdAndUpdate(id, { username })
+//         .then(() => res.redirect('/profile'))
+//         .catch((err) => next(err));
+//     }
+//   });
+
+
+
 
   module.exports = router;
