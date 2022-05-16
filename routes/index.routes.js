8994d0a48aa51569c645aad939506f4cbbd08506
Marina Palma <marina_palma@hotmail.com>
2022-05-16 12:22:24 +0100

    update user ok

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+
+  res.render("index", {user: req.session.user});
 });
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,41 +1,42 @@
-const router = require('express').Router();
-const User = require('../models/User.model');
+const router = require("express").Router();
+const User = require("../models/User.model");
+const fileUploader = require("../config/cloudinary.config");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/profile", (req, res, next) => {
-   console.log(req.session)
-    res.render("users/profile", { user: req.session.user });
-      
+router.get("/profile/:id", (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id).then((user) => {
+    res.render("users/profile", { user });
   });
+});
 
-router.get('/profile/:id/edit', (req, res, next) => {
+router.get("/profile/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => {
+      res.render("users/profile-edit", user);
+    })
+    .catch((err) => next(err));
+});
+
+router.post(
+  "/profile/:id/edit",
+  fileUploader.single("profile-image"),
+  (req, res, next) => {
     const { id } = req.params;
-    User.findById(id)
-      .then((user) => {
-        res.render('users/profile-edit', user);
-      })
-      .catch((err) => next(err));
-  });
 
-//   router.post('/:id/edit', fileUploader.single('profile-image'), (req, res, next) => {
-//     const { id } = req.params;
-
-//     const { username } = req.body;
-   
+    const { username } = req.body;
+    console.log(req.file)
   
-//     if (req.file) {
-//       User.findByIdAndUpdate(id, { username, imageUrl: req.file.path })
-//         .then(() => res.redirect('/profile'))
-//         .catch((err) => next(err));
-//     } else {
-//       User.findByIdAndUpdate(id, { username })
-//         .then(() => res.redirect('/profile'))
-//         .catch((err) => next(err));
-//     }
-//   });
+      User.findByIdAndUpdate(id, { username, imageUrl: req.file.path })
+        .then((updatedUser) => {
+            console.log(updatedUser)
+          res.redirect(`/profile/${id}`);
+        })
+        .catch((err) => next(err));
+   
+  }
+);
 
-
-
-
-  module.exports = router;
+module.exports = router;
